c11426883d5249434cedac44d535bd4d3c9e2797
wjcho <wj930303@gmail.com>
2024-07-13 23:41:34 +0900

    Feat: CASE 관련 내용

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -21,7 +21,7 @@ public class JpqlMain {
                 em.persist(team);
 
                 member.setUsername("member" + i);
-                member.setAge(i+16);
+                member.setAge(6*i+16);
                 member.setTeam(team);
                 member.setType(MemberType.USER);
                 em.persist(member);
@@ -31,34 +31,66 @@ public class JpqlMain {
             em.clear();
 
             System.out.println("======== 1 START ========");
-            String query = "select m from Member m where m.type = jpql.MemberType.USER";
+            String query1 =
+                    "SELECT " +
+                            "CASE " +
+                            "WHEN m.age <= 19 THEN '학생요금' " +
+                            "WHEN m.age >= 60 THEN '경로요금' " +
+                            "ELSE '일반요금' " +
+                            "END " +
+                            "FROM Member m";
 
-            List<Member> result = em.createQuery(query, Member.class)
+            List<String> result = em.createQuery(query1, String.class)
                     .getResultList();
 
             System.out.println("result size: " + result.size());
 
-            for (Member member : result) {
-                System.out.println("member = " + member + ", team name: " + member.getTeam().getName());
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
             System.out.println("======== 1 END ========");
 
 
 
-            // 패키지명을 파라미터 바인딩으로 해결
+            // COALESCE
             System.out.println("======== 2 START ========");
-            String query2 = "select m from Member m where m.type = :userType";
 
-            List<Member> result2 = em.createQuery(query2, Member.class)
-                    .setParameter("userType", MemberType.USER)
+            Member memberNoName1 = new Member();
+            em.persist(memberNoName1);
+
+            Member memberNoName2 = new Member();
+            em.persist(memberNoName2);
+
+            Member memberHasName = new Member();
+            memberHasName.setUsername("홍길동");
+            em.persist(memberHasName);
+
+            String query2 = "SELECT COALESCE (m.username, '이름 없는 회원') FROM Member m";
+            List<String> result2 = em.createQuery(query2, String.class)
                     .getResultList();
 
-            System.out.println("result size: " + result.size());
+            System.out.println("result2 size: " + result2.size());
 
-            for (Member member : result2) {
-                System.out.println("member = " + member + ", team name: " + member.getTeam().getName());
+            for (String s : result2) {
+                System.out.println("s = " + s);
             }
             System.out.println("======== 2 END ========");
+
+
+            // NULLIF
+            System.out.println("======== 3 START ========");
+
+            String query3 = "SELECT NULLIF (m.username, 'member5') FROM Member m";
+            List<String> result3 = em.createQuery(query3, String.class)
+                    .getResultList();
+
+            System.out.println("result3 size: " + result3.size());
+
+            for (String s : result3) {
+                System.out.println("s = " + s);
+            }
+            System.out.println("======== 3 END ========");
+
 
             tx.commit();
         } catch (Exception e) {
