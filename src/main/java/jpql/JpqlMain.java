5b86f12c3b6fbcb7142b9d39fd415537f2e0f590
wjcho <wj930303@gmail.com>
2024-07-13 17:48:23 +0900

    Chore: 페이징 메서드 테스트

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -14,50 +14,27 @@ public class JpqlMain {
 
         try {
 
-            Member member1 = new Member();
-            member1.setUsername("홍길동");
-            member1.setAge(15);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("김김김");
-            member2.setAge(22);
-            em.persist(member2);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-            Member findMember = members.get(0);
-            findMember.setAge(20);
+            System.out.println("result.size() = " + result.size());
 
-            for (Member member : members) {
-                System.out.println("member name: " + member.getUsername() + ", member age: " + member.getAge());
+            for (Member member : result) {
+                System.out.println(member);
             }
-
-            List<Team> team = em.createQuery("select m.team from Member m", Team.class)
-                    .getResultList();
-
-            List<Address> addressList = em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList();
-            for (Address address : addressList) {
-                System.out.println("address = " + address);
-            }
-
-            em.createQuery("select m.username, m.age from Member m")
-                            .getResultList();
-
-            List<MemberDTO> result = em.createQuery(
-                    "select new jpql.MemberDTO(m.username, m.age) from Member m",
-                            MemberDTO.class)
-                    .getResultList();
-
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO username = " + memberDTO.getUsername());
-            System.out.println("memberDTO age = " + memberDTO.getAge());
-
+            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
